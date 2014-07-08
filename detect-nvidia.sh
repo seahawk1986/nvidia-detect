da33b1c37379282350c757aac78bc540bed5f3f8
Alexander Grothe <alexander@VDR.fritz.box>
2014-07-08 19:19:31 +0200

    add detect-nvidia.sh

---
+++ detect-nvidia.sh
@@ -0,0 +1,29 @@
+latest=0
+nvidia304=0
+nvidia173=0
+
+for device in /proc/bus/pci/??/*; do
+  Vendor=`hexdump -s 0 -n 2 -e '1/2 "%04x"' $device`
+  Device=`hexdump -s 2 -n 2 -e '1/2 "%04x"' $device`
+  Class=`hexdump -s 10 -n 2 -e '1/2 "%04x"' $device`
+  if [[ "$Class" == 0300 && "$Vendor" == "10de" ]]; then
+    echo "got nVidia graphics card: $Device"
+    if grep -Fxq "^0x$Device" nvidia_304.x_device_ids; then
+      nvidia304=1
+    elif grep -Fxq "^0x$Device" nvidia_173_device_ids; then
+      nvidia173=1
+    else
+      latest=1
+    fi
+    
+  fi
+
+done
+
+if [[ $latest == 1 ]]; then
+    echo "nvidia-graphics-drivers-331-updates"
+elif [[ $nvidia304 == 1 ]]; then
+    echo "nvidia-graphics-drivers-304-updates"
+elif [[ $nvidia173 == 1 ]]; then
+    echo "nvidia-graphics-drivers-173"
+fi
